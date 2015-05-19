0fb161f9db75103303c2978b7d8a07dccbc8d60e
bhennes2 <bhennes2@gmail.com>
2015-05-19 13:26:38 -0500

    Small updates

--- app/models/copilot/content.rb
+++ app/models/copilot/content.rb
@@ -1,6 +1,7 @@
 module Copilot
   class Content < ActiveRecord::Base
     validates :slug, presence: true, uniqueness: true
+    validates :value, presence: true
 
     scope :for_page, -> (controller, action) { where("slug LIKE ?", "%#{controller}.#{action}%") }
 
