d9215089269e99a678f658f1b2a83c73cee38d9a
bhennes2 <bhennes2@gmail.com>
2015-06-03 16:53:59 -0500

    Have file uploads open in a new tab

--- app/models/copilot/file.rb
+++ app/models/copilot/file.rb
@@ -1,21 +1,18 @@
 module Copilot
   class File < Copilot::Content
-    serialize :value, Hash
 
     has_attached_file :attachment
+    validates :attachment, :attachment_presence => true
+    do_not_validate_attachment_file_type :attachment
 
     def url
-      value[:url]
-    end
-
-    def content
-      value[:content]
+      attachment.url
     end
 
     def render(options={})
       class_names = (options[:class_names] || []).join(' ')
       contenteditable = options[:contenteditable]
-      "<a href='#{url}' #{contenteditable} data-copilot-id='#{id}' data-copilot-slug='#{slug}' class='copilot-editable #{class_names}'>#{content}</a>".html_safe
+      "<a href='#{url}' target='_blank' #{contenteditable} data-copilot-id='#{id}' data-copilot-slug='#{slug}' class='copilot-editable #{class_names}'>#{value}</a>".html_safe
     end
   end
 end
